7a23cbecb1750891c1b3659254e512a080c6d187
Yasuhiro Noake <a1728yn@aiit.ac.jp>
2017-06-04 02:20:21 +0000

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,8 +20,9 @@ end
 
 class ActionDispatch::IntegrationTest
 
-  def log_in_as(user, password: 'password')
+  def log_in_as(user, password: 'password', remember_me: '1')
     post login_path, params: { session: { email: user.email,
-                                          password: password} }
+                                          password: password,
+                                          remember_me: remember_me } }
   end
 end
